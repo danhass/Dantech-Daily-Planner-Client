32e9c52dc757b8215e72beda335db1c5fea8a300
Dan Hass <hass.dan@gmail.com>
2022-08-03 21:15:24 -0500

    Init DanTech Daily Planner app

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { GoogleApiService, UserInfo } from './google-api.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'DanTech\'s Daily Planner';
+
+  userInfo?: UserInfo;
+
+  constructor(private readonly googleApi: GoogleApiService) {
+    googleApi.userProfileSubject.subscribe( info => {
+      this.userInfo = info;
+    })
+  }
+
+  isLoggedIn(): boolean {
+    return this.googleApi.isLoggedIn();
+  }
+
+  logout() {
+    this.googleApi.signOut();
+  }
+
+  accessToken(): string {
+    return this.googleApi.accessToken();
+  }
+}
+  
