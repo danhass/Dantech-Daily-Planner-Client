09950490ed34f4081c341a3b874c73875b4e2b90
danhass <hass.dan@gmail.com>
2023-12-28 20:42:05 -0600

    Added net carbs to keto.

--- src/app/dt-keto/dt-keto.component.ts
+++ src/app/dt-keto/dt-keto.component.ts
@@ -9,6 +9,7 @@ export class DtKetoComponent implements OnInit {
   ketoFatGrams: number = 0;
   ketoProteinGrams: number = 0;
   ketoCarbGrams: number = 0;
+  ketoFiberGrams: number = 0
   isKeto: boolean = false;
   isKetoHiPro: boolean = false;
 
@@ -19,8 +20,7 @@ export class DtKetoComponent implements OnInit {
   calcKeto(): void {
     console.log ("fired");
     let totalCal = this.ketoFatGrams * 9 + this.ketoCarbGrams * 4 + this.ketoProteinGrams * 4;    
-    this.isKeto = (totalCal == 0 || ((this.ketoCarbGrams * 4)/totalCal <= .1 && (this.ketoProteinGrams * 4)/totalCal <= .2));
-    this.isKetoHiPro = (totalCal == 0 || ((this.ketoCarbGrams * 4)/totalCal <= .05 && (this.ketoProteinGrams * 4)/totalCal <= .35));
-    console.log(totalCal, this.isKeto);
+    this.isKeto = (totalCal == 0 || ((this.ketoCarbGrams - this.ketoFiberGrams) * 4)/totalCal <= .1 && (this.ketoProteinGrams * 4)/totalCal <= .2);
+    this.isKetoHiPro = (totalCal == 0 || (((this.ketoCarbGrams - this.ketoFiberGrams) * 4)/totalCal <= .05 && (this.ketoProteinGrams * 4)/totalCal <= .35));
   }
 } 
